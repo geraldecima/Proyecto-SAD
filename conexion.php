0ea74c7eadbbcf6f14dff98909492e4529596363
Geraldine <48465385+geraldecima@users.noreply.github.com>
2019-10-25 15:58:44 -0300

    Delete conexion.php

--- conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$server="localhost";
-	$username="root";
-	$password="";
-	$db="sad";	
-	$con=mysqli_connect($server,$username,$password,$db) or die("No se ha podido establecer la conexion");
-?>
